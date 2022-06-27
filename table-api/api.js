1cd531fcda3a0a53da4a59489ef58b14947b905a
cchenchen <cchen@caci.com>
2022-06-27 13:08:49 -0400

    Added video streaming and video transition events and broadcasts.

--- table-api/api.js
+++ table-api/api.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -17,17 +18,77 @@ const watchoutParams = {
   timeout: 10000
 }
 
-async function videotransition(){
+async function videotransitionin(){
   try{
     await conn.connect(watchoutParams);
   } catch (err) {
     console.log("error: ", err);
   }
 
-  await conn.send('authenticate 1');
-  await conn.send('run ASCEND');
-  await new Promise(r => setTimeout(r, 3000));
+  try{
+    await conn.send('authenticate 1');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+  try{
+    await conn.send('kill ASCEND');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+  try{
+    await conn.send('run ASCEND');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+  try{
+    await new Promise(r => setTimeout(r, 500));
+    await conn.send('run VIDEO');
+  } catch (err) {
+    console.log("error: ", err);
+  }
 }
+
+async function videotransitionout(){
+  try{
+    await conn.connect(watchoutParams);
+  } catch (err) {
+    console.log("error: ", err);
+  }
+
+  try{
+    await conn.send('authenticate 1');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+  try{
+    await conn.send('kill SWIRREL');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+  try{
+    await conn.send('run SWIRREL');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+  try{
+    await new Promise(r => setTimeout(r, 250));
+    await conn.send('run VIDEO');
+  } catch (err) {
+    console.log("error: ", err);
+  }
+}
+
+app.get('play/:video_id' (req, res) => {
+  const path = __dirname + "/videos/2000s_v9_061322_JF.mp4";
+  const stat = fs.statSync(path);
+  const filesize = stat.size;
+  const head = {
+    'Content-Length': filesize,
+    'Content-Type': 'video/mp4'
+  };
+  res.writeHead(200, head);
+  fs.createReadStream(path).pipe(res);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -39,6 +100,11 @@ io.on('connection', (socket) => {
   // handle events
   socket.on("start video", (video_id) => {
     console.log("new video playing: ", video_id);
+    videotransitionin();
+
+    setTimeout(() => {
+      console.log("transtion done, playing video");
+      socket.broadcast.emit('play', video_id);}, 6200);
   });
 
   socket.on("pause video", () => {
@@ -49,9 +115,13 @@ io.on('connection', (socket) => {
     console.log("resuming video");
   });
 
-  socket.on("restart video", () => {
+  socket.on("replay video", () => {
     console.log("restarting video");
-    socket.emit('test client event');
+    videotransitionout();
+
+    setTimeout(() => {
+      console.log("transition done, restarting video");
+      socket.broadcast.emit('restart', 1)}, 6000);
   });
 });
 
