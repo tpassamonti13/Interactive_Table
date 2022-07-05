f09a7e1bac0911c5ebc049e0e08a90dae964a480
cchenchen <cchen@caci.com>
2022-07-05 09:59:02 -0400

    Added timeout test to Video.

--- table-client/src/components/Video.js
+++ table-client/src/components/Video.js
@@ -1,32 +1,48 @@
 import React from 'react';
 import { socket } from '../App';
+import { useNavigate as navigate } from 'react-router-dom';
 
 //const socket = io('192.168.0.45:3456');
+var videoPlaying = false;
+var timeout;
 
 function StartVideo(video_id) 
 {
   socket.emit('start video', video_id);
   console.log('playing video: ', video_id);
+  videoPlaying = true;
 }
 
 function PauseVideo() 
 {
   socket.emit('pause video', "");
+  videoPlaying = false;
+  stopped();
 }
 
 function ResumeVideo() 
 {
   socket.emit('resume video', "");
+  videoPlaying = true;
+  stopped();
 }
 
-function ReplayVideo() 
-{
-  socket.emit('replay video',"");
-}
-
-function init()
+function InitVideo()
 {
 	console.log('video initialized');
+  socket.on('stop', () => 
+  {
+    timeout = setTimeout(() => 
+    {
+      socket.broadcast.emit('reset video');
+      navigate('/');
+    }, 30000);
+  });
 }
 
-export {StartVideo, PauseVideo, ResumeVideo, ReplayVideo};
+function stopped()
+{
+  clearTimeout(timeout);
+}
+
+export {StartVideo, PauseVideo, ResumeVideo, ReplayVideo, videoPlaying };
